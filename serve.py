f1014e553959b05255a0f38ced748ba89ca50e8e
Jeff McGee <JeffAMcGee@gmail.com>
2011-02-27 01:06:10 -0600

    moved everything around, updated README to reflect changes

--- serve.py
+++ serve.py
@@ -7,6 +7,6 @@ class Root(object):
     pass
 
 if __name__ == '__main__':
-    cherrypy.config.update("etc/engine.conf")
+    cherrypy.config.update("etc/crowdy.conf")
     cherrypy.tree.mount(api,"/api/1",config="etc/api.conf")
     cherrypy.quickstart(Root(),"/",config="etc/static.conf")
