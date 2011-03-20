c15dae90a65a0a86e5882e563a8c9995591ea53d
Jeff McGee <JeffAMcGee@gmail.com>
2011-03-20 00:09:46 -0500

    user/tweets api looks good - needs testing

--- api/user.py
+++ api/user.py
@@ -1,6 +1,7 @@
 import cherrypy
 from cherrypy import tools
 from api.models import User,Tweet,Edges
+from utils import parse_date
 
 @cherrypy.expose
 @tools.json_out()
@@ -14,11 +15,13 @@ def edges(uid):
 
 @cherrypy.expose
 @tools.json_out()
-def tweets(uid,start=None,end=None):
-    #FIXME: write parse_time
-    start = parse_time(start)
-    end = parse_time(end)
+def tweets(uid,start_date=None,end_date=None,limit=100):
+    start = parse_date(start_date)
+    end = parse_date(end_date)
+    limit = int(limit) if limit else None
     tweets = Tweet.find(
         (Tweet.user_id==int(uid)) &
-        (Tweet.created_at.range(start,end)))
+        (Tweet.created_at.range(start,end)),
+        limit=limit,
+        sort=Tweet._id)
     return [t.to_d() for t in tweets]
--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,9 @@
 from time import mktime
-
+from datetime import datetime
 
 def to_epoch(dt):
     if dt!=None: return mktime(dt.timetuple())
 
 def parse_date(dstr):
     if not dstr: return None
-    if dstr[0]=="[":
+    return datetime.utcfromtimestamp(float(dstr))
