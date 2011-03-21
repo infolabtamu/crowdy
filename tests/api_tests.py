d078e45c69a9a54daf073e112f5da97c297498c3
Jeff McGee <JeffAMcGee@gmail.com>
2011-03-20 20:26:46 -0500

    added test_user_tweets

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -20,6 +20,20 @@ class TestAPI(unittest.TestCase):
         jeff = api.user.id('106582358')
         self.failUnlessEqual(jeff['sn'],'JeffAMcGee')
         self.failUnlessRaises(cherrypy.HTTPError, api.user.id, '12')
+    
+    def test_user_tweets(self):
+        #tweets(uid,start_date=None,end_date=None,limit=100):
+        jan5 = '1294185600'
+        jan16 = '1295136000'
+        before = api.user.tweets('106582358',end_date=jan5)
+        self.failUnlessEqual(len(before),3)
+        after = api.user.tweets('106582358',start_date=jan16)
+        self.failUnlessEqual(len(after),4)
+        mid = api.user.tweets('106582358',start_date=jan5,end_date=jan16)
+        self.failUnlessEqual(len(mid),12)
+        lim = api.user.tweets('106582358',limit=5)
+        self.failUnlessEqual(len(lim),5)
+
 
 if __name__ == '__main__':
     unittest.main()
