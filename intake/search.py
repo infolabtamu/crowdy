60ff6353f7ba354b84906d9c61b292f420559c50
Jeff McGee <JeffAMcGee@gmail.com>
2011-04-18 22:03:24 -0500

    created CrowdIndexer and CrowdIndexFilter classes

--- intake/search.py
+++ intake/search.py
@@ -10,7 +10,7 @@ Unique keys?
 import sys
 sys.path.append('../')
 import lucene, os
-from lucene import Document, Field, RAMDirectory, IndexWriter, StandardAnalyzer, Version, IndexSearcher, TopScoreDocCollector, QueryParser, SimpleFSDirectory, Term
+from lucene import Document, Field, IndexWriter, StandardAnalyzer, Version, IndexSearcher, TopScoreDocCollector, QueryParser, SimpleFSDirectory, Term, JavaError
 from etc.settings import settings
 from api.models import Crowd, Tweet
 from intake.module import CrowdFilter
@@ -21,7 +21,44 @@ class CrowdFields:
     id='id'
     text='text'
 
-class CrowdsSearch(CrowdFilter):
+
+class CrowdIndexer():
+    def __enter__(self):
+        if not CrowdsSearch.initialized:
+            CrowdsSearch.initialize()
+        mkdir = not os.path.exists(settings.lucene_index_dir)
+        self.writer = IndexWriter(CrowdsSearch.index,
+            CrowdsSearch.analyzer,
+            mkdir,
+            IndexWriter.MaxFieldLength.UNLIMITED)
+        return self
+
+    def addCrowd(self,id,text):
+        doc = Document();
+        doc.add(Field(CrowdFields.id, id, Field.Store.YES, Field.Index.NOT_ANALYZED))
+        doc.add(Field(CrowdFields.text, text, Field.Store.YES, Field.Index.ANALYZED))
+        try:
+            CrowdsSearch.getCrowds(id, CrowdFields.id)
+            self.writer.updateDocument(Term(CrowdFields.id, id), doc)
+        except JavaError:
+            self.writer.addDocument(doc)
+
+    def __exit__(self, type, value, traecback):
+        self.writer.close()
+
+
+class CrowdIndexFilter(CrowdFilter): 
+    def cfilter(self, crowds):
+        with CrowdIndexer() as index:
+            for crowd in crowds:
+                print "indexing %s"%crowd._id
+                tweets = crowd.tweets(limit=None)
+                text = "\n".join(t.text for t in tweets)
+                index.addCrowd(crowd._id,text)
+                yield crowd
+
+
+class CrowdsSearch:
     initialized = False
     
     @staticmethod
@@ -30,32 +67,6 @@ class CrowdsSearch(CrowdFilter):
         lucene.initVM()
         CrowdsSearch.index = SimpleFSDirectory(lucene.File(settings.lucene_index_dir))
         CrowdsSearch.analyzer = StandardAnalyzer(Version.LUCENE_CURRENT);
-    
-    def cfilter(self, crowds):
-        def _isDocumentUpdate(crowd):
-            try:
-                return CrowdsSearch.getCrowds(crowd[CrowdFields.id], CrowdFields.id)
-            except:
-                return False
-        if not CrowdsSearch.initialized:
-            CrowdsSearch.initialize()
-        mkdir = not os.path.exists(settings.lucene_index_dir)
-        writer = IndexWriter(CrowdsSearch.index, CrowdsSearch.analyzer, mkdir, IndexWriter.MaxFieldLength.UNLIMITED)
-
-        for crowd in crowds:
-            print "indexing %s"%crowd._id
-            tweets = crowd.tweets(limit=None)
-            text = "\n".join(t.text for t in tweets)
-            doc = Document();
-            doc.add(Field(CrowdFields.id, crowd._id, lucene.Field.Store.YES, lucene.Field.Index.NOT_ANALYZED))
-            doc.add(Field(CrowdFields.text, text, lucene.Field.Store.YES, lucene.Field.Index.ANALYZED))
-            if _isDocumentUpdate(crowd):
-                writer.updateDocument(Term(CrowdFields.id, crowd._id), doc)
-            else:
-                writer.addDocument(doc)
-            yield crowd
-        writer.close()
-    
 
     @staticmethod
     def getCrowds(query, field = CrowdFields.text): 
--- tests/intake/search_tests.py
+++ tests/intake/search_tests.py
@@ -7,7 +7,7 @@ import sys, os
 sys.path.append('../../')
 import unittest
 from etc.settings import settings
-from tools.search import CrowdsSearch
+from intake.search import CrowdsSearch, CrowdIndexer
 
 testIndexFolder = '/tmp/search_test'
 
@@ -17,11 +17,12 @@ class TwitterSearchTest(unittest.TestCase):
         os.system('rm -rf %s'%settings.lucene_index_dir)
         
     def test_generalCrowdSearch(self):
-        CrowdsSearch.addCrowd({'id': '1', 'text': 'Lucene in Action'})
-        CrowdsSearch.addCrowd({'id': '2', 'text': 'Lucene for Dummies'})
-        CrowdsSearch.addCrowd({'id': '3', 'text': 'Managing Gigabytes'})
-        CrowdsSearch.addCrowd({'id': '4', 'text': 'The Art of Computer Science'})
-        CrowdsSearch.addCrowd({'id': '5', 'text': 'The Art of Computer Science and Engineering'})
+        with CrowdIndexer() as index:
+            index.addCrowd('1',u'Lucene in Action')
+            index.addCrowd('2',u'Lucene for Dummies')
+            index.addCrowd('3',u'Managing Gigabytes')
+            index.addCrowd('4',u'The Art of Computer Science')
+            index.addCrowd('5',u'The Art of Computer Science and Engineering')
         self.assertEqual([{'text': u'Lucene in Action', 'id': u'1'}, {'text': u'Lucene for Dummies', 'id': u'2'}], CrowdsSearch.getCrowds('lucene'))
         self.assertEqual([{'text': u'The Art of Computer Science', 'id': u'4'}, {'text': u'The Art of Computer Science and Engineering', 'id': u'5'}], CrowdsSearch.getCrowds('science'))
     
@@ -29,12 +30,13 @@ class TwitterSearchTest(unittest.TestCase):
         docBeforeUpdate = {'id': '1', 'text': 'Lucene in Action'}
         docAfterUpdate = {'text': 'Lucene for Dummies', 'id': '1'}
         
-        CrowdsSearch.addCrowd(docBeforeUpdate)
-        self.assertEqual([docBeforeUpdate], CrowdsSearch.getCrowds('action'))
-        
-        CrowdsSearch.addCrowd(docAfterUpdate)
-        self.assertNotEqual([docAfterUpdate], CrowdsSearch.getCrowds('action'))
-        self.assertEqual([docAfterUpdate], CrowdsSearch.getCrowds('dummies'))
+        with CrowdIndexer() as index:
+            index.addCrowd(**docBeforeUpdate)
+            self.assertEqual([docBeforeUpdate], CrowdsSearch.getCrowds('action'))
+            
+            index.addCrowd(**docAfterUpdate)
+            self.assertNotEqual([docAfterUpdate], CrowdsSearch.getCrowds('action'))
+            self.assertEqual([docAfterUpdate], CrowdsSearch.getCrowds('dummies'))
     
     def tearDown(self):
         os.system('rm -rf %s'%testIndexFolder)
