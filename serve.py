9d8a09f8e341e44f0425776624d385a6426bbf65
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-19 18:14:56 -0500

    modified code for fcgi

--- serve.py
+++ serve.py
@@ -27,8 +27,26 @@ if __name__ == '__main__':
             name=settings.mongo_database,
             host=settings.mongo_host)
         cherrypy.config.update("etc/crowdy.conf")
-        cherrypy.tree.mount(api,"/api/1",config="etc/api.conf")
-        cherrypy.quickstart(web,"/",config="etc/web.conf")
+        # Code added for fastcgi
+        if "--fastcgi" in sys.argv:
+#            app = cherrypy.tree.mount(WebPWMan())
+            app = cherrypy.tree.mount(api,"/api/1",config="etc/api.conf")
+            # CherryPy autoreload must be disabled for the flup server to work
+            
+            cherrypy.config.update({'engine.autoreload_on':False})
+            cherrypy.config.update({
+                    "tools.sessions.on": True,
+                    "tools.sessions.timeout": 5,
+                    "log.screen": False,
+                    "log.access_file": "/tmp/cherry_access.log",
+                    "log.error_file": "/tmp/cherry_error.log",
+            })
+            from flup.server.fcgi import WSGIServer
+            cherrypy.config.update({'engine.autoreload_on':False})
+            WSGIServer(app).run()
+        else:
+            cherrypy.tree.mount(api,"/api/1",config="etc/api.conf")
+            cherrypy.quickstart(web,"/",config="etc/web.conf")
     except:
         traceback.print_exc()
         pdb.post_mortem(sys.exc_info()[2])
