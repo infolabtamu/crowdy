be7b8536572431f8346873df8ef99bb3ccc445e2
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-20 12:48:20 -0500

    modified conf

--- hello.py
+++ hello.py
@@ -1,46 +1,46 @@
-#'''
-#Created on Jul 20, 2011
-#
-#@author: kykamath
-#'''
-#import cherrypy, sys
-#
-#class HelloWorld:
-#    def index(self):
-#        return "Hello world!"
-#    index.exposed = True
-#    
-#if "--fastcgi" in sys.argv:
-##    app = cherrypy.tree.mount(HelloWorld())
-##    # CherryPy autoreload must be disabled for the flup server to work
-##    cherrypy.config.update('hello.conf')
-##    cherrypy.config.update({'engine.autoreload_on':False})
-##    cherrypy.config.update({
-##            "tools.sessions.on": True,
-##            "tools.sessions.timeout": 5,
-##            "log.screen": False,
-##            "log.access_file": "/tmp/cherry_access.log",
-##            "log.error_file": "/tmp/cherry_error.log",
-##    })
-##    from flup.server.fcgi import WSGIServer
-##    cherrypy.config.update({'engine.autoreload_on':False})
-##    WSGIServer(app).run()
-#    cherrypy.tree.mount(HelloWorld())
-#    # CherryPy autoreload must be disabled for the flup server to work
-#    cherrypy.config.update({'engine.autoreload_on':False})
-#else:
-#    cherrypy.quickstart(HelloWorld(), config="hello.conf")
+'''
+Created on Jul 20, 2011
 
-
-#!/usr/bin/python
-import cherrypy
+@author: kykamath
+'''
+import cherrypy, sys
 
 class HelloWorld:
-    """Sample request handler class."""
     def index(self):
         return "Hello world!"
     index.exposed = True
+    
+if "--fastcgi" in sys.argv:
+#    app = cherrypy.tree.mount(HelloWorld())
+#    # CherryPy autoreload must be disabled for the flup server to work
+#    cherrypy.config.update('hello.conf')
+#    cherrypy.config.update({'engine.autoreload_on':False})
+#    cherrypy.config.update({
+#            "tools.sessions.on": True,
+#            "tools.sessions.timeout": 5,
+#            "log.screen": False,
+#            "log.access_file": "/tmp/cherry_access.log",
+#            "log.error_file": "/tmp/cherry_error.log",
+#    })
+#    from flup.server.fcgi import WSGIServer
+#    cherrypy.config.update({'engine.autoreload_on':False})
+#    WSGIServer(app).run()
+    cherrypy.tree.mount(HelloWorld())
+    # CherryPy autoreload must be disabled for the flup server to work
+    cherrypy.config.update({'engine.autoreload_on':False})
+else:
+    cherrypy.quickstart(HelloWorld(), config="hello.conf")
 
-cherrypy.tree.mount(HelloWorld())
-# CherryPy autoreload must be disabled for the flup server to work
-cherrypy.config.update({'engine.autoreload_on':False})
+
+##!/usr/bin/python
+#import cherrypy
+#
+#class HelloWorld:
+#    """Sample request handler class."""
+#    def index(self):
+#        return "Hello world!"
+#    index.exposed = True
+#
+#cherrypy.tree.mount(HelloWorld())
+## CherryPy autoreload must be disabled for the flup server to work
+#cherrypy.config.update({'engine.autoreload_on':False})
