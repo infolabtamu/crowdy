8438e770bd09aea5803e14b7f54a41fb8a586b7f
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-20 11:18:31 -0500

    playing with fcgi

---
+++ hello.py
@@ -0,0 +1,28 @@
+'''
+Created on Jul 20, 2011
+
+@author: kykamath
+'''
+import cherrypy, sys
+
+class HelloWorld:
+    def index(self):
+        return "Hello world!"
+    index.exposed = True
+    
+if "--fastcgi" in sys.argv:
+    app = cherrypy.tree.mount(HelloWorld())
+    # CherryPy autoreload must be disabled for the flup server to work
+    cherrypy.config.update({'engine.autoreload_on':False})
+    cherrypy.config.update({
+            "tools.sessions.on": True,
+            "tools.sessions.timeout": 5,
+            "log.screen": False,
+            "log.access_file": "/tmp/cherry_access.log",
+            "log.error_file": "/tmp/cherry_error.log",
+    })
+    from flup.server.fcgi import WSGIServer
+    cherrypy.config.update({'engine.autoreload_on':False})
+    WSGIServer(app).run()
+else:
+    cherrypy.quickstart(HelloWorld(), config="hello.conf")
